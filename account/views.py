475ca470bd06a2c2ab87d9a70b9c4a06e4c865dc
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-22 15:25:25 +0100

    django messages

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
 
@@ -58,6 +59,9 @@ def edit(request):
     if user_form.is_valid() and profile_form.is_valid():
       user_form.save()
       profile_form.save()
+      messages.success(request, 'Profile updated successfully')
+    else:
+      messages.error(request, 'Error updating your profile')
 
   else:
     user_form = UserEditForm(instance=request.user)
