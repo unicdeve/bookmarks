51e450a9f85bd6f8721c110cf7b9405ae50c8b79
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-25 14:57:45 +0100

    generic activity stream: users actions

--- account/views.py
+++ account/views.py
@@ -10,6 +10,9 @@ from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from django.contrib.auth.models import User
 
+from actions.utils import create_action
+from actions.models import Action
+
 
 def user_login(request):
   if request.method == 'POST':
@@ -32,7 +35,16 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-  return render(request, 'account/dashboard.html', { 'section': 'dashboard'})
+  # Display all actions by default
+  actions = Action.objects.exclude(user=request.user)
+  follwing_ids = request.user.following.values_list('id', flat=True)
+
+  if follwing_ids:
+    # If user is following others, retrieve only their actions
+    actions = actions.filter(user_id__in=follwing_ids)
+  actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+
+  return render(request, 'account/dashboard.html', { 'section': 'dashboard', 'actions': actions})
 
 
 def register(request):
@@ -44,6 +56,7 @@ def register(request):
 
       new_user.save()
       Profile.objects.create(user=new_user)
+      create_action(new_user, 'has created an account')
       return render(request, 'account/register_done.html', { 'new_user': new_user })
 
   else:
@@ -100,6 +113,7 @@ def user_follow(request):
       user = User.objects.get(id=user_id)
       if action == 'follow':
         Contact.objects.get_or_create(user_from=request.user, user_to=user)
+        create_action(request.user, 'is following', user)
       else:
         Contact.objects.filter(user_from=request.user, user_to=user).delete()
       return JsonResponse({'status': 'ok'})
--- images/views.py
+++ images/views.py
@@ -9,6 +9,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 
+from actions.utils import create_action
+
 
 @login_required
 def image_create(request):
@@ -20,6 +22,7 @@ def image_create(request):
       # assign current user to the image model
       new_item.user = request.user
       new_item.save()
+      create_action(request.user, 'bookmarked image', new_item)
       messages.success(request, "Image added successfully")
 
       return redirect(new_item.get_absolute_url())
@@ -50,6 +53,7 @@ def image_like(request):
       image = Image.objects.get(id=image_id)
       if action == 'like':
         image.users_like.add(request.user)
+        create_action(request.user, 'likes', image)
       else:
         image.users_like.remove(request.user)
       return JsonResponse({'status': 'ok'})
